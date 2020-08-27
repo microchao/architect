3474a4c5ab8bb1a3dd97cc28b22dbfbf2021adc8
xiaochao.shen <xiaochao.shen@baozun.com>
2020-08-27 16:48:49 +0800

    add BubbleSort

---
+++ algorithms/src/main/java/com/sxc/architect/algorithms/sort/BubbleSort.java
@@ -0,0 +1,20 @@
+package com.sxc.architect.algorithms.sort;
+
+/**
+ * 冒泡排序
+ * @Date 2020/08/27
+ */
+public class BubbleSort {
+    public static void sort(int[] array) {
+        int length = array.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                int temp = array[i];
+                if(array[j] > array[i]) {
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ algorithms/src/test/java/com/sxc/architect/algorithms/sort/BubbleSortTest.java
@@ -0,0 +1,31 @@
+package com.sxc.architect.algorithms.sort;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Random;
+
+public class BubbleSortTest {
+
+    public int[] generateArray() {
+        int[] array = new int[1000000];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+        return array;
+    }
+
+    @Test
+    public void sort() {
+        int[] array = generateArray();
+        long startTime = System.currentTimeMillis();
+        BubbleSort.sort(array);
+        long endTime = System.currentTimeMillis();
+        System.out.println("BubbleSort time:" + (endTime - startTime) + "/ms, array size:" + array.length);
+        for (int i = 0; i < array.length-1; i++) {
+            Assert.assertEquals(array[i] <= array[i+1],true);
+        }
+    }
+}
