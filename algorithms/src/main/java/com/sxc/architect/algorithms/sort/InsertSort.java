c5fe623a36ea0cd052654bce547f6a589ac94909
xiaochao.shen <xiaochao.shen@baozun.com>
2020-09-01 17:52:42 +0800

    updates comment

--- algorithms/src/main/java/com/sxc/architect/algorithms/sort/InsertSort.java
+++ algorithms/src/main/java/com/sxc/architect/algorithms/sort/InsertSort.java
@@ -8,12 +8,16 @@ public class InsertSort implements Sort{
     public void sort(int[] array) {
         for (int i = 1; i < array.length; i++) {
             int j = i;
-            int key = array[j]; // 拿到当前需要往前插入的
-            while(j > 0 && array[j-1] > key) { // 关键点：每次比较都是要和key比较，比key大的往后挪动
-                array[j] = array[j-1]; // 比当前需要插入的大的往后挪
+            int key = array[i];
+            /**
+             * 关键点在于每次都是跟key比较
+             * 比key大的往后挪
+             */
+            while(j>0 && array[j-1] > key) {
+                array[j] = array[j-1];
                 j--;
             }
-            array[j] = key; // 已经空出的位置
+            array[j] = key;
         }
     }
 
