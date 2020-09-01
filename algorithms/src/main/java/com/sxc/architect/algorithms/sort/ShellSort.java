5ce4a247e5aa07c91ce5a83b92eba8be3eca2f63
xiaochao.shen <xiaochao.shen@baozun.com>
2020-09-01 14:51:53 +0800

    希尔排序完成

--- algorithms/src/main/java/com/sxc/architect/algorithms/sort/ShellSort.java
+++ algorithms/src/main/java/com/sxc/architect/algorithms/sort/ShellSort.java
@@ -2,40 +2,24 @@ package com.sxc.architect.algorithms.sort;
 
 /**
  * 希尔排序
+ * 是插入排序的改进，解决插入排序每次只能移动一步的缺点
  * @Date 20200828
  */
 public class ShellSort implements Sort{
-
-    public  void sort(int[] array) {
-        int gap = array.length/2-1;
-        for(int i = gap; gap >0;  gap = gap/2) { //这一层算出gap
-            for(int j = 0; j< array.length; j = j+gap) { // 找出所有gap的数字
-                int key = array[j];
-                int k = j;
-                while(k>0 && array[k-gap] > key) {
-                    array[k] = array[k-gap];
-                    k=k-gap;
+   public  void sort(int[] array) {
+        int n = array.length;
+        for (int gap = n/2; gap > 0; gap=gap/2) {
+            for(int i = gap;i < n;i++) {
+                int key = array[i];
+                int j = i;
+                while(j>=gap && array[j-gap] > key) {
+                    array[j] = array[j-gap];
+                    j= j-gap;
                 }
-                array[k]=key;
+                array[j] = key;
             }
         }
     }
-
-//    public void sort(int arrayToSort[]) {
-//        int n = arrayToSort.length;
-//
-//        for (int gap = n / 2; gap > 0; gap /= 2) {
-//            for (int i = gap; i < n; i++) {
-//                int key = arrayToSort[i];
-//                int j = i;
-//                while (j >= gap && arrayToSort[j - gap] > key) {
-//                    arrayToSort[j] = arrayToSort[j - gap];
-//                    j -= gap;
-//                }
-//                arrayToSort[j] = key;
-//            }
-//        }
-//    }
 
     public String getName() {
         return "希尔排序";
