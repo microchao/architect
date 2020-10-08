311c47a45a0c8bbcfdec00d7d7517081ab247cae
microchao225@gmail.com <microchao225@gmail.com>
2020-10-08 18:08:46 +0800

    test change

--- algorithms/src/test/java/com/sxc/architect/algorithms/sort/SortTest.java
+++ algorithms/src/test/java/com/sxc/architect/algorithms/sort/SortTest.java
@@ -13,15 +13,15 @@ public class SortTest extends BaseTest{
     @Before
     public void before() {
         sortList = new ArrayList<Sort>();
-//        sortList.add(new BubbleSort());
-//        sortList.add(new InsertSort());
-//        sortList.add(new ShellSort());
+        sortList.add(new BubbleSort());
+        sortList.add(new InsertSort());
+        sortList.add(new ShellSort());
         sortList.add(new QuickSort());
     }
 
     @Test
     public void sort() {
-        super.ARRAY_LENGTH = 9;
+        super.ARRAY_LENGTH = 9999;
         int[] array = super.generateArray();
         for (Sort sort : sortList) {
             int[] sortArray = super.copyArray(array);
