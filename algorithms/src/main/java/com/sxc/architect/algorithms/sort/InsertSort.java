2c91103dc628a1c041f45a32e8a2773d80373331
microchao225@gmail.com <microchao225@gmail.com>
2020-10-09 11:13:33 +0800

    insertsort add comment

--- algorithms/src/main/java/com/sxc/architect/algorithms/sort/InsertSort.java
+++ algorithms/src/main/java/com/sxc/architect/algorithms/sort/InsertSort.java
@@ -10,8 +10,9 @@ public class InsertSort implements Sort{
             int j = i;
             int key = array[i];
             /**
-             * 关键点在于每次都是跟key比较
-             * 比key大的往后挪
+             * 关键点：
+             * 1.每次都是跟key比较比key大的往后挪
+             * 2.while循环只比较前面的大小，因为前面的已经排过序了，不用顾及再前面的
              */
             while(j>0 && array[j-1] > key) {
                 array[j] = array[j-1];
